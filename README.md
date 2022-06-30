8c9eaf9ee1c1e10ee33e4e5cad2e08d8ded92ade
mason369 <maosenyang00@gmail.com>
2022-06-30 12:24:44 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## 蔡徐坤邀请你视频通话-Cai Xukun invites you to make a video call
 纯属娱乐，蔡徐坤邀请你视频通话
 乐到你了的话别忘了右上角的star哦  
+
 ![](/img/wKgBOV4VsWyAB9cMAAtAhfXnV4g3012.no)  
 [Live Demo]()
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-window.onload = function () {
-//   const music = new Audio("1call.mp3");
-//   music.play();
-//   music.loop = true;
-alert('sdfasdf')
-};
+// window.onload = function () {
+// //   const music = new Audio("1call.mp3");
+// //   music.play();
+// //   music.loop = true;
+// alert('sdfasdf')
+// };
