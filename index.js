82dabd9e51510e5a13b3d00b63ddedd43704b871
mason369 <maosenyang00@gmail.com>
2022-07-02 18:42:09 +0800

    update

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 // //   music.loop = true;
 // alert('sdfasdf')
 // };
+window.onload = function () {
+    const music = new Audio(
+      "https://mason369.github.io/Cai-Xukun-invites-you-to-make-a-video-call/audio/1call.mp3"
+    );
+    music.play();
+    music.loop = true;
+
+    var points=document.getElementsByTagName('span');
+    for(var i=0;i<points;i++){
+        
+    }
+       
+  };
