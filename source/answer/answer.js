d34634240664c324786803a2a0af59343f5afa6a
mason369 <maosenyang00@gmail.com>
2022-07-02 19:43:05 +0800

    add music

---
+++ source/answer/answer.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const music = new Audio(
+      "https://mason369.github.io/Cai-Xukun-invites-you-to-make-a-video-call//audio/无奈owo - [全损音质]鸡你太美.mp3"
+    );
+    music.play();
+    music.loop = true;
+
+}
