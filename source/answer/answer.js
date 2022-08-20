9215352b916e74f9988003099e353adb54ccedd4
mason369 <maosenyang00@gmail.com>
2022-08-20 11:21:06 +0800

    Update answer.js

--- source/answer/answer.js
+++ source/answer/answer.js
@@ -1,9 +1,10 @@
 window.onload = function () {
-  setTimeout(function () {
+  function countSecond() {
     const music = new Audio(
       "https://mason369.github.io/Cai-Xukun-invites-you-to-make-a-video-call/audio/757104139_nb3-1-30216.mp3"
     );
     music.play();
-    music.loop = true; // 延迟处理的事件
-  }, 1000);
+    music.loop = true;
+    setTimeout("countSecond()", 1000);
+  }
 };
