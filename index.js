c84568136702c64e0212a51c60e165a410df5d7d
mason369 <maosenyang00@gmail.com>
2022-08-20 17:15:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-// window.onload = function () {
-// //   const music = new Audio("1call.mp3");
-// //   music.play();
-// //   music.loop = true;
-// alert('sdfasdf')
-// };
+
 window.onload = function () {
-    const music = new Audio(
-      "https://mason369.github.io/Cai-Xukun-invites-you-to-make-a-video-call/audio/1call.mp3"
-    );
-    music.play();
-    music.loop = true;
-
-    // var points=document.getElementsByTagName('span');
-    // for(var i=0;i<points.length;i++){
-    //     points[i].style.display='none';
-    //     sleep(500);
-    //     points[i].style.display='block';
-    //     if(i===3){
-    //         i=0;
-    //     }
-    // }
-    // function sleep(millisecond) {
-    //     return new Promise(resolve => {
-    //         setTimeout(() => {
-    //             resolve()
-    //         }, millisecond)
-    //     })
-    // }
-
+  const music = new Audio(
+    "https://mason369.github.io/Cai-Xukun-invites-you-to-make-a-video-call//audio/1call.mp3"
+  );
+  music.play();
+  music.loop = true;
 
 }
- 
