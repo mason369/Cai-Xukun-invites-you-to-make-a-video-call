645b9d11381685adb1ca62460e4abcde079418f0
mason369 <1960638223@qq.com>
2023-05-23 11:06:13 +0800

    updaet：修改js交互

--- index.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function () {
-  const music = new Audio(
-    "https://mason369.github.io/Cai-Xukun-invites-you-to-make-a-video-call//audio/1call.mp3"
-  );
-  music.play();
-  music.loop = true;
-
-}
