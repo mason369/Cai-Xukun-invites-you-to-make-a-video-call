4e5653d5f3bf5f7975dbc4ea1fe62e004edcf6bf
mason369 <1960638223@qq.com>
2023-05-23 10:31:43 +0800

    update：删除冗余文件

--- source/answer/answer.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-  const music = new Audio(
-    "https://mason369.github.io/Cai-Xukun-invites-you-to-make-a-video-call/audio/757104139_nb3-1-30216.mp3"
-  );
-  music.play(setTimeout("countSecond()", 1000));
-  music.loop = true;
-};
